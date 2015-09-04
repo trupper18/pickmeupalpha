bb584a0817c67e83790705e33f812a9bf458b835
trupper18 <trupper18@verizon.net>
2015-09-04 12:49:06 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+//COMMENT
+echo "<title>Index Page</title>"; 
+echo "<h1>The Index Page</h1>"; 
+echo "<p>";
+echo "<a href='login.php'>Login Page</a>";
+echo "<br>";
+echo "<a href='register.php'>Register Page</a>";
+echo "<br>";
+echo "<a href='createEvent.php'>Create Event Page</a>";
+echo "<br>";
+echo "<a href='categoriesAndTopics.php'>Category/Topic Page</a>";
+?>
