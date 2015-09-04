a351fff6d7346da14e60fa7382753a3a5f59a8aa
trupper18 <trupper18@verizon.net>
2015-09-04 12:46:20 -0500

    Create categoriesAndTopics.php

---
+++ categoriesAndTopics.php
@@ -0,0 +1,9 @@
+<?php
+echo "<form action='makeCatAndTop.php' method='post'>"; 
+echo "<input type='text' name='category' placeholder='Category'/><br>";
+echo "Or <br>";
+echo "<input type='text' name='topic' placeholder='Topic'/><br>";
+echo "<input type='text' name='topic_category' placeholder='Topic Category'/><br>";
+echo "<input type='submit' name='submit' value='submit'>"; //Submit button.
+
+?>
